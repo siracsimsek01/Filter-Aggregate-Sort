b515b9281e74f43cf0c3fa65b725e6c7d0296c4a
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2023-11-28 18:10:05 +0000

    file updated without lambda function

--- sorting.py
+++ sorting.py
@@ -1,20 +1,36 @@
 from players import players
 
+
+#Helper function to get the sorting key
+def get_key(player, sort_key):
+    return player[sort_key]
+
 # Function to sort the list by anything in ascending or descending order
 
 def sorting_players(players, sort_key):
-
-    # Sorting players using the sorted function and a custom sorting key
-    sorted_players = sorted(players, key=lambda player: player[sort_key])
-
-    # Extracting sorted names and surnames
-    sorted_names = [f"{player['name']} {player['surname']} {player[sort_key]}" for player in sorted_players]
     
-    # to avoid printing the name twice if the sort key is name or surname
-    if sort_key == 'name' or sort_key == 'surname':
-        sorted_names = [f"{player['name']} {player['surname']}" for player in sorted_players]
+    # Custom key for sorting
     
-    return sorted_names
+    def custom_key(player):
+        return get_key(player, sort_key)
+    
+    # Using the sorted function with the custom key function
+    
+    sorted_players = sorted(players, key=custom_key)
+    
+    # Excluding sorted names and surnames from the list
+    
+    sorted_names = []
+    for player in sorted_players:
+        name_string = f"{player['name']} {player['surname']}"
+        if sort_key not in ['name', 'surname']:
+            name_string += f" {player[sort_key]}"
+        sorted_names.append(name_string)
+
+    return sorted_names    
+
+
+
 
 # Example Usage
 sorted_by_age = sorting_players(players, 'age') 
